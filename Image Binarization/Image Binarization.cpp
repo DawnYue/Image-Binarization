756cd978ec35b3162af8d69018cc95923e47c97c
晨曦 <1179348863@qq.com>
2020-03-13 14:37:02 +0800

    练习3

--- Image Binarization/Image Binarization.cpp
+++ Image Binarization/Image Binarization.cpp
@@ -7,53 +7,48 @@ using namespace std;
 
 
 string window_name = "binaryMat";
+void threshold_Mat(int th, void*data)
+{
+	Mat src= *(Mat*)(data);
+	Mat dst;
+
+	threshold(src, dst, th, 255, 0);
+
+	imshow(window_name, dst);
+}
+/*
+
+CV_EXPORTS int createTrackbar(const String& trackbarname, const String& winname,
+	int* value, int count,
+	TrackbarCallback onChange = 0,
+	void* userdata = 0);
+*/
+
 int main()
 {  
 	Mat srcMat;
 	Mat gryMat;
 	int lowTh = 30;
 	int maxTh = 255;
-	cv::Mat srcMat = imread("E:\\4.png");
-
-	//创建滑动条来控制阈值
-	createTrackbar("模式",
-		WINDOW_NAME, &g_nThresholdType,
-		4, on_Threshold);
-
-	VideoCapture cap(0);
-	double scale=0.5;
-	//0-180肤色
-	double i_minH = 0;
-	double i_maxH = 20;
-	//0-255
-	double i_minS = 43;
-	double i_maxS = 255;
-	//0-255
-	double i_minV = 55;
-	double i_maxV = 255;
-
-	while (1)
-	{   
-		Mat frame;
-		Mat hsvMat;
-		Mat detectMat;
-
-		cap >> frame;
-		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
-		Mat rFrame = Mat(ResImgSiz, frame.type());
-		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
-		
-		threshold(frame, rFrame, 100, 255, THRESH_BINARY);//阈值函数
-		adaptiveThreshold(frame, rFrame,255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV,15,0);//区域自适应二值化
-		
-		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
-
-		rFrame.copyTo(detectMat);
-		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
-		imshow("while :in the range", detectMat);
-		imshow("frame", rFrame);
-		waitKey(30);
+	srcMat = imread("E:\\4.png");
+	if (!srcMat.data) 
+	{ 
+		cout << "打开失败" << endl; return 0; 
 	}
 
+	//原图的灰度图
+	cvtColor(srcMat, gryMat, COLOR_RGB2GRAY);
+
+	
+	imshow(window_name, gryMat);
+	createTrackbar("threshold",
+		window_name,
+		&lowTh, 
+		maxTh, 
+		threshold_Mat,
+		&gryMat);
+	waitKey(0);//等待用户按键
+
+	return 0;
 
 }
